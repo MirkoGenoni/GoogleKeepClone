b6789d9606b055ec6be8a1175b3a91172e4abe84
mirko <mirko.genoni@mail.polimi.it>
2022-09-22 11:13:56 +0200

    Basic note distribution on page and data handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,116 @@
 <script>
-	export let name;
+	let firstcolumn=[]
+	let secondcolumn=[]
+	let thirdcolumn=[]
+	let fourthcolumn=[]
+	let fifthcolumn=[]
+	let sixthcolumn = []
+	let columncollect=[
+		firstcolumn, secondcolumn, thirdcolumn, fourthcolumn, fifthcolumn, sixthcolumn
+	]
+
+	let column = 0;
+	let postitid = 0;
+	let titleAdd="";
+	let bodyAdd="";
+
+	function addNote(){
+		columncollect[column].push({id: postitid, title: titleAdd, body: bodyAdd});
+		if(column<5){
+			column++;
+		}else{
+			column=0;
+		}
+		postitid++;
+		columncollect[column]=columncollect[column];
+		console.log(columncollect)
+	}
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="navbar">
+		<button on:click={addNote}>Click</button>
+	</div>
+	<div id="notecontainer">
+		{#each columncollect as column}
+			<div class="column">
+				{#each column as postit}
+					<div class="postit">
+						<textarea class="title" bind:value={postit.title}></textarea>
+						<textarea class="body" bind:value={postit.body}></textarea>
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	#navbar{
+		width: 100%;
+		height: 5vh;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	#notecontainer{
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: 95vh;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.column{
+		display: flex;
+		flex-direction: column;
+		column-gap: 3vw;
+		width:20%;
+		height: fit-content;
+		margin-right: 2vw;
+	}
+
+	.postit{
+		display: flex;
+		flex-direction: column;
+		width:100%;
+		height: 25vh;
+		margin-bottom: 2vh;
+		margin-right: 2vw;
+		border-style: solid;
+		border-radius: 10px;
+		border-width: 1px;
+	}
+	
+	.title{
+		resize: none;
+		overflow: clip;
+		width: 100%;
+		height: 20%;
+		border-top-left-radius: 15px;
+		border-top-right-radius: 15px;
+		border-top-width: 0px;
+		border-right-width: 0px;
+		border-left-width: 0px;
+		border-bottom-style: solid;
+		border-bottom-color: black;
+		outline: 0px;
+		margin: 0px;
+	}
+
+	.body{
+		resize: none;
+		overflow: clip;
+		vertical-align: text-top;
+		width: 100%;
+		height: 80%;
+		border: none;
+		outline: none;
+		margin: 0px;
+		margin-bottom: 1.5vh;
+		padding: 1vw;
+		border-radius: 15px;
+	}
+
+	.body:focus{
+		overflow: auto;
 	}
 </style>
