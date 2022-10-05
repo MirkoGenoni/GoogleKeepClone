7fde34382a52b203681f6e45a6742fb6d7d00ac9
mirko <mirko.genoni@mail.polimi.it>
2022-10-05 18:16:16 +0200

    New code organization

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </svelte:head>
 <script>
-    import { tick } from "svelte";
-	import Palette from "./BackgroundColor.svelte"
+    import NewNote from "./NewNote.svelte"
 
 	let firstcolumn=[]
 	let secondcolumn=[]
@@ -18,81 +17,7 @@
 		firstcolumn, secondcolumn, thirdcolumn, fourthcolumn, fifthcolumn, sixthcolumn
 	]
 	let allElements = [];
-
-	let postitid = 0;
-	let titleAdd="";
-	let bodyAdd="";
-	
-	let clicked = false;
-	let dimensionintBody;
-	let dimensionintTitle;
-	let styleBody = "--height: 20px;";
-	let styleTitle = "--height: 24px;";
-	let newpostTitle;
-	let newpostBody;
-
-	let isPalette = false;
-	let currentBackground ="--background-color: #ffffff;";
-
-	let image;
-	let submitimage;
-	let isImage = false;
-
-	function setImage(e){
-		isImage=true;
-		let curr = e.target.files[0];
-		let reader = new FileReader();
-		reader.readAsDataURL(curr);
-		reader.onload = e => {
-			image = e.target.result;
-		}
-		clicked= true;
-	}
-	const setBackground = (color) => {
-		currentBackground = "--background-color: "+ color +";";
-		console.log(currentBackground)
-	}
-
-	function setClick(){
-		clicked= true;
-	}
-
-	function setPalette(){
-		if(isPalette){
-			isPalette = false;
-		} else {
-			isPalette = true;
-		}
-	}
-
-	function handleout(event){
-		let nodes;
-		let array = [];
-		if(isPalette===true){
-			nodes = document.getElementById("optionscontainer").childNodes;
-			nodes.forEach((value)=>{array.push(value.id)})
-			array.push("nocoloricon");
-			console.log(array);
-		}
-		if(event.target.id!=="toolbaropenpalette" && event.target.id!=="optionscontainer" && !array.includes(event.target.id) && isPalette===true){
-			console.log(event.target)
-			isPalette=false;
-		}
-	}
-
-	async function dynamicResizeBody(props){
-		if(props=="body"){
-			styleBody = "--height: 0;";
-			await tick();
-			dimensionintBody = newpostBody.scrollHeight;
-			styleBody= "--height: " + dimensionintBody.toString() + "px;";
-		} else {
-			styleTitle = "--height: 0;";
-			await tick();
-			dimensionintTitle = newpostTitle.scrollHeight;
-			styleTitle= "--height: " + dimensionintTitle.toString() + "px;";
-		}
-	}
+	let bo = 0;
 
 	function clearColumnState(currentValue){
 		currentValue.length=0;
@@ -102,109 +27,27 @@
 		columncollect[index%6].push(currentValue);
 	}
 
-	function closeNote(){
-		clicked=false;
-		newpostTitle.value="";
-		newpostBody.valute="";
-		titleAdd="";
-		bodyAdd="";
-		styleBody = "--height: 20px;";
-		styleTitle = "--height: 24px;";
-		currentBackground ="--background-color: #ffffff;";
-		isPalette=false;
-		isImage=false;
-		image=""; 
-		submitimage.value="";
-	}
-
 	function addNote(){
-		if(bodyAdd!=="" || titleAdd!==""){
-			columncollect.forEach(clearColumnState);
-			allElements.unshift({id: postitid, title: titleAdd, body: bodyAdd});
-			allElements.forEach(populateColumns);
-			columncollect=columncollect;
-			postitid++;
-		}
-
-		closeNote();
-	}
-
-	function deleteimage(){
-		isImage=false;
-		image="";
-		submitimage.value="";
-	}
-
-	function outsideClick(node){
-		const handleClick = (event) => {
-			if (!node.contains(event.target) && clicked===true && event.target.id!="toolbaropenpalette") {
-				node.dispatchEvent(new CustomEvent("outclick"));
-			}
-		};
-		
-		document.addEventListener("click", handleClick, true);
-
-		return {
-		destroy() {
-			document.removeEventListener("click", handleClick, true);
-			}
-		};
+		columncollect.forEach(clearColumnState);
+		allElements.forEach(populateColumns);
+		columncollect=columncollect;
 	}
 
 </script>
 
 <main>
-	<div id="newNote">
-		<div id="start" class="newpostit" use:outsideClick on:outclick={addNote} on:click={(e)=> handleout(e)} style={currentBackground}>
-			<div id="titlebodyimage">
-			{#if isImage}
-				<div id="imagecontainer">
-					<img id="uploadedimage" src={image}/>
-					<div id="deleteimagecontainer">
-						<span id="deleteimage" class="material-symbols-outlined" on:click={deleteimage}>delete</span>
-					</div>
-				</div>
-			{/if}
-			<div id="titlewithimage">
-				<input style="display:none" type="file" accept=".jpg, .jpeg, .png" bind:this={submitimage} on:change={(e)=>{setImage(e);}}/>
-				<div id="newtitlecontainer" class="textcontainer" style={"--width: " + (clicked===false ? "80%" : "100%")} on:click={setClick}>
-					<textarea placeholder={clicked===false ? "Scrivi una nota..." : "Titolo..."} id="newtitle" class="title" bind:this={newpostTitle} bind:value={titleAdd} on:input={()=>dynamicResizeBody("title")} style={styleTitle+currentBackground}></textarea>
-				</div>
-				{#if !clicked}
-				<div id="iconcontainer" on:click={()=>{submitimage.click()}}>
-					<span id="toolbarclosedimage" class="material-symbols-outlined">image</span>
-				</div>
-				{/if}
-			</div>
-			{#if clicked}
-				<div id="newbodycontainer" class="textcontainer">
-					<textarea placeholder="Scrivi una nota..." id="newbody" class="body" bind:this={newpostBody} bind:value={bodyAdd} on:input={()=>dynamicResizeBody("body")} style={styleBody+currentBackground + (isImage === true ? "--max-height: none" : "--max-height: 340px")}></textarea>
-				</div>
-			{/if}
-			</div>
-			{#if clicked}
-				<div id="toolbarcontainer">
-					<div id="tools">
-						<span id="toolbaropenimage" class="material-symbols-outlined" on:click={()=>{submitimage.click()}}>image</span>
-						<span id="toolbaropenpalette" class="material-symbols-outlined" on:click={setPalette}>palette</span>
-						{#if isPalette}
-							<Palette setBackground={setBackground}/>
-						{/if}
-					</div>
-					<button id="closenote" on:click={closeNote} style={currentBackground}>Chiudi</button>
-				</div>
-			{/if}
-		</div>
-	</div>
+	<NewNote {allElements} {addNote}/>
 	<div id="notecontainer">
 		{#each columncollect as column}
 			<div class="column">
 				{#each column as postit}
 					<div class="postit">
 						<div class="textcontainer">
-							<textarea class="title" bind:value={postit.title}></textarea>
+							<textarea readonly id="postittitle" class="title" bind:value={postit.title}></textarea>
 						</div>
-						<textarea class="body" bind:value={postit.body}></textarea>
+						<div id="bodycontainer" class="textcontainer">
+							<textarea readonly id="postitbody" class="body" bind:value={postit.body}></textarea>
+						</div>
 					</div>
 				{/each}
 			</div>
@@ -220,231 +63,12 @@
 		font-family: "Google Sans", Roboto, Arial, sans-serif;
 	}
 
-	/*FORM NUOVA NOTA*/
-	/*Container div per centrare form newpostit*/
-	#newNote{
-		display: flex;
-		justify-content: center;
-		width: 100%;
-		height: fit-content;
-		max-height: 800px;
-		margin-top: 32px;
-		margin-bottom: 16px;
-	}
-
-	/**/
-	#titlebodyimage{
-		height: fit-content;
-		max-height: 724px;
-		overflow: scroll;
-	}
-
-	#imagecontainer{
-		position: relative;
-		height: fit-content;
-		width: fit-content;
-	}
-
-	#imagecontainer:hover > #deleteimagecontainer{
-		opacity:40%;
-	}
-	#imagecontainer:hover > #deleteimagecontainer:hover{
-		opacity:100%;
-	}
-	/*Nuova immagine nella nota*/
-	#uploadedimage{
-		width: 100%;
-		height: 100%;
-	}
-
-	#deleteimage{
-		color:white;
-	}
-	#deleteimagecontainer{
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		position: absolute;
-		background-color: black;
-		width:34px;
-		height: 34px;
-		right: 15px;
-		bottom: 15px;
-		z-index: 2;
-		opacity: 0%;
-	}
-
-	/*Box nuova nota, contiene form per titolo e boty e tasto per confermare*/
-	.newpostit{
-		box-sizing: border-box;
-		width: 598px;
-		white-space: 0px;
-		height: fit-content;
-		box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%);
-		border: solid 1px transparent;
-		border-radius: 8px;
-		border-color: var(--background-color);
-		background-color: var(--background-color);
-	}
-
-	/*TEXTAREA TITOLO*/
-	/*Barra titolo chiusa con icona imagine*/
-	#titlewithimage{
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		justify-content:space-between;
-	}
-
-	/*Container per dare padding alla textarea del titolo*/
-	#newtitlecontainer{
-		width: var(--width);
-		display: flex;
-		height: auto;
-		padding-top: 10px;
-		padding-bottom: 10px;
-		padding-left: 15px;
-		padding-right: 15px;
-	}
-	/*Proprietà aggiuntive per titolo nuovo postit*/
-	#newtitle{
-		height: var(--height);
-		background-color: var(--background-color);
-		font-size: 16px;
-		color: rgba(0,0,0,0.702);
-		letter-spacing: 0.00625em;
-		font-weight: 500;
-		line-height: 1.5rem;
-		max-height: 340px;
-		overflow: auto;
-	}
-
-	/*PROPRIETÀ GENERALI ICONE GOOGLE*/
-	.material-symbols-outlined{
-		user-select: none;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		border-radius: 100%;
-		opacity: 80%;
-	}
-	.material-symbols-outlined:hover{
-		background-color: rgba(95,99,104,0.157);
-	}
-
-	/*NEWNOTECHIUSA*/
-	/*Container icone barra titolo*/
-	#iconcontainer{
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 20%;
-		height: 44px;
-	}
-	/*Icona immagine*/
-	#toolbarclosedimage{
-		width: 44px;
-		height: 44px;
-	}
-
-	/*NEWNOTE APERTA*/
-	/*Container barra inferiore newnote*/
-	#toolbarcontainer{
-		position: relative;
-		/*margin-top: 32px;*/
-		margin-left: 15px;
-		margin-right: 15px;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-	}
-
-	/*Container strumenti*/
-	#tools{
-		display: flex;
-		flex-direction: row;
-	}
-
-	/*Icona immagine*/
-	#toolbaropenimage{
-		margin-right: 8px;
-		margin-right: 8px;
-		width: 34px;
-		height: 34px;
-	}
-
-	/*Icona palette*/
-	#toolbaropenpalette{
-		margin-right: 8px;
-		margin-left: 8px;
-		width: 34px;
-		height: 34px;
-	}
-
-	/*TEXTAREA BODY*/
-	/*Container per dare padding alla textarea del body*/
-	#newbodycontainer{
-		display: flex;
-		height: fit-content;
-		padding-top: 12px;
-		padding-bottom: 12px;
-		padding-left: 15px;
-		padding-right: 15px;
-	}
-	/*Proprietà font placeholder body*/
-	#newbody::placeholder{
-		font-size: 14px;
-		color: rgba(0,0,0,0.702);
-		font-weight: 500;
-		letter-spacing: 0.01785714em;
-		line-height: 1.25rem;
-	}
-	/*Proprietà aggiuntive per body nuovo postit*/
-	#newbody{
-		height: var(--height);
-		background-color: var(--background-color);
-		max-height: var(--max-height);
-		overflow: auto;
-		color: #202124;
-		font-size: 14px;
-		letter-spacing: .01428571em;
-		font-weight: 400;
-		line-height: 1.25rem;
-		padding: 0px;
-		border-radius: 0px;
-		margin: 0px;
-	}
-	
-	/*BOTTONE CHIUDI NOTA*/
-	/*Button chiudi nota*/
-	#closenote{
-		background-color: white;
-		border: transparent;
-		height: 36px;
-		padding: 8px 24px;
-		border-radius: 4px;
-		margin-top: 2px;
-		margin-bottom: 2px;
-		font-size: 14px;
-		color: rgba(0,0,0,.87);
-		font-weight: 500;
-		line-height: 1.25rem;
-		background-color: var(--background-color);
-	}
-	#closenote:hover{
-		background-color: rgba(95,99,104,0.039);
-	}
-	#closenote:active{
-		background-color: rgba(95,99,104,0.161)!important;
-	}
-
-
 	#notecontainer{
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
 		width: 100%;
-		height: 95vh;
+		height: fit-content;
 	}
 
 	.column{
@@ -493,13 +117,17 @@
 		white-space: pre-wrap;
 	}
 
+	#bodycontainer{
+		padding-top: 0px;
+	}
+
 	.body{
 		font-size: 14px;
 		resize: none;
 		overflow: clip;
 		vertical-align: text-top;
 		width: 100%;
-		height: 165px;
+		height: auto;
 		overflow: auto;
 		border: none;
 		outline: none;
@@ -512,5 +140,9 @@
 
 	.body:focus{
 		overflow: auto;
+	}
+
+	#postitbody{
+		user-select: none;
 	}
 </style>
---
+++ src/NewNote.svelte
@@ -0,0 +1,410 @@
+<script>
+    import { tick } from "svelte";
+	import Palette from "./BackgroundColor.svelte"
+
+    let postitid = 0;
+    let bodyAdd = "";
+    let titleAdd= "";
+
+    export let allElements;
+    export let addNote;
+
+    let clicked = false;
+	let dimensionintBody;
+	let dimensionintTitle;
+	let styleBody = "--height: 20px;";
+	let styleTitle = "--height: 24px;";
+	let newpostTitle;
+	let newpostBody;
+
+	let isPalette = false;
+	let currentBackground ="--background-color: #ffffff;";
+
+	let image;
+	let submitimage;
+	let isImage = false;
+
+	function setImage(e){
+		isImage=true;
+		let curr = e.target.files[0];
+		let reader = new FileReader();
+		reader.readAsDataURL(curr);
+		reader.onload = e => {
+			image = e.target.result;
+		}
+		clicked= true;
+	}
+
+    function closeNote(){
+		clicked=false;
+		newpostTitle.value="";
+		newpostBody.valute="";
+		titleAdd="";
+		bodyAdd="";
+		styleBody = "--height: 20px;";
+		styleTitle = "--height: 24px;";
+		currentBackground ="--background-color: #ffffff;";
+		isPalette=false;
+		isImage=false;
+		image=""; 
+		submitimage.value="";
+	}
+
+	const setBackground = (color) => {
+		currentBackground = "--background-color: "+ color +";";
+		console.log(currentBackground)
+	}
+
+	function setClick(){
+		clicked= true;
+	}
+
+	function setPalette(){
+		if(isPalette){
+			isPalette = false;
+		} else {
+			isPalette = true;
+		}
+	}
+
+	function handleout(event){
+		let nodes;
+		let array = [];
+		if(isPalette===true){
+			nodes = document.getElementById("optionscontainer").childNodes;
+			nodes.forEach((value)=>{array.push(value.id)})
+			array.push("nocoloricon");
+			console.log(array);
+		}
+		if(event.target.id!=="toolbaropenpalette" && event.target.id!=="optionscontainer" && !array.includes(event.target.id) && isPalette===true){
+			console.log(event.target)
+			isPalette=false;
+		}
+	}
+
+	async function dynamicResizeBody(props){
+		if(props=="body"){
+			styleBody = "--height: 0;";
+			await tick();
+			dimensionintBody = newpostBody.scrollHeight;
+			styleBody= "--height: " + dimensionintBody.toString() + "px;";
+		} else {
+			styleTitle = "--height: 0;";
+			await tick();
+			dimensionintTitle = newpostTitle.scrollHeight;
+			styleTitle= "--height: " + dimensionintTitle.toString() + "px;";
+		}
+	}
+
+    function deleteimage(){
+		isImage=false;
+		image="";
+		submitimage.value="";
+	}
+
+    function propagateContent(){
+        if(bodyAdd!="" || titleAdd!=""){
+            allElements.unshift({id: postitid, title: titleAdd, body: bodyAdd});
+            postitid++;
+        }
+        closeNote();
+        console.log(allElements)
+        addNote();
+    }
+
+	function outsideClick(node){
+        console.log("qui")
+		const handleClick = (event) => {
+			if (!node.contains(event.target) && clicked===true && event.target.id!="toolbaropenpalette") {
+				node.dispatchEvent(new CustomEvent("outclick"));
+			}
+		};
+		
+		document.addEventListener("click", handleClick, true);
+
+		return {
+		destroy() {
+			document.removeEventListener("click", handleClick, true);
+			}
+		};
+	}
+</script>
+
+    <div id="newNote">
+		<div id="start" class="newpostit" use:outsideClick on:outclick={propagateContent} on:click={(e)=> handleout(e)} style={currentBackground}>
+			<div id="titlebodyimage">
+			{#if isImage}
+				<div id="imagecontainer">
+					<img id="uploadedimage" alt ="immagine caricata" src={image}/>
+					<div id="deleteimagecontainer">
+						<span id="deleteimage" class="material-symbols-outlined" on:click={deleteimage}>delete</span>
+					</div>
+				</div>
+			{/if}
+			<div id="titlewithimage">
+				<input style="display:none" type="file" accept=".jpg, .jpeg, .png" bind:this={submitimage} on:change={(e)=>{setImage(e);}}/>
+				<div id="newtitlecontainer" class="textcontainer" style={"--width: " + (clicked===false ? "80%" : "100%")} on:click={setClick}>
+					<textarea placeholder={clicked===false ? "Scrivi una nota..." : "Titolo..."} id="newtitle" class="title" bind:this={newpostTitle} bind:value={titleAdd} on:input={()=>dynamicResizeBody("title")} style={styleTitle+currentBackground}></textarea>
+				</div>
+				{#if !clicked}
+				<div id="iconcontainer" on:click={()=>{submitimage.click()}}>
+					<span id="toolbarclosedimage" class="material-symbols-outlined">image</span>
+				</div>
+				{/if}
+			</div>
+			{#if clicked}
+				<div id="newbodycontainer" class="textcontainer">
+					<textarea placeholder="Scrivi una nota..." id="newbody" class="body" bind:this={newpostBody} bind:value={bodyAdd} on:input={()=>dynamicResizeBody("body")} style={styleBody+currentBackground + (isImage === true ? "--max-height: none" : "--max-height: 340px")}></textarea>
+				</div>
+			{/if}
+			</div>
+			{#if clicked}
+				<div id="toolbarcontainer">
+					<div id="tools">
+						<span id="toolbaropenimage" class="material-symbols-outlined" on:click={()=>{submitimage.click()}}>image</span>
+						<span id="toolbaropenpalette" class="material-symbols-outlined" on:click={setPalette}>palette</span>
+						{#if isPalette}
+							<Palette setBackground={setBackground}/>
+						{/if}
+					</div>
+					<button id="closenote" on:click={closeNote} style={currentBackground}>Chiudi</button>
+				</div>
+			{/if}
+		</div>
+	</div>
+
+<style>
+    /*FORM NUOVA NOTA*/
+	/*Container div per centrare form newpostit*/
+	#newNote{
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		height: fit-content;
+		max-height: 800px;
+		margin-top: 32px;
+		margin-bottom: 16px;
+	}
+
+	/**/
+	#titlebodyimage{
+		height: fit-content;
+		max-height: 724px;
+		overflow: scroll;
+        border-top-right-radius: 8px;
+        border-top-left-radius: 8px;
+    }
+
+	#imagecontainer{
+		position: relative;
+		height: fit-content;
+		width: fit-content;
+	}
+
+	#imagecontainer:hover > #deleteimagecontainer{
+		opacity:40%;
+	}
+	#imagecontainer:hover > #deleteimagecontainer:hover{
+		opacity:100%;
+	}
+	/*Nuova immagine nella nota*/
+	#uploadedimage{
+		width: 100%;
+		height: 100%;
+	}
+
+	#deleteimage{
+		color:white;
+	}
+	#deleteimagecontainer{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		background-color: black;
+		width:34px;
+		height: 34px;
+		right: 15px;
+		bottom: 15px;
+		z-index: 2;
+		opacity: 0%;
+	}
+
+	/*Box nuova nota, contiene form per titolo e boty e tasto per confermare*/
+	.newpostit{
+		box-sizing: border-box;
+		width: 598px;
+		white-space: 0px;
+		height: fit-content;
+		box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%);
+		border: solid 1px transparent;
+		border-radius: 8px;
+		border-color: var(--background-color);
+		background-color: var(--background-color);
+	}
+
+	/*TEXTAREA TITOLO*/
+	/*Barra titolo chiusa con icona imagine*/
+	#titlewithimage{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content:space-between;
+	}
+
+	/*Container per dare padding alla textarea del titolo*/
+	#newtitlecontainer{
+		width: var(--width);
+		display: flex;
+		height: auto;
+		padding-top: 10px;
+		padding-bottom: 10px;
+		padding-left: 15px;
+		padding-right: 15px;
+	}
+	/*Proprietà aggiuntive per titolo nuovo postit*/
+	#newtitle{
+		height: var(--height);
+		background-color: var(--background-color);
+		font-size: 16px;
+		color: rgba(0,0,0,0.702);
+		letter-spacing: 0.00625em;
+		font-weight: 500;
+		line-height: 1.5rem;
+		max-height: 340px;
+		overflow: auto;
+        resize: none;
+        border: none;
+        width: 100%;
+		padding: 0px;
+		outline: 0px;
+		margin: 0px;
+		border: none;
+		white-space: pre-wrap;
+	}
+
+	/*PROPRIETÀ GENERALI ICONE GOOGLE*/
+	.material-symbols-outlined{
+		user-select: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 100%;
+		opacity: 80%;
+	}
+	.material-symbols-outlined:hover{
+		background-color: rgba(95,99,104,0.157);
+	}
+
+	/*NEWNOTECHIUSA*/
+	/*Container icone barra titolo*/
+	#iconcontainer{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 20%;
+		height: 44px;
+	}
+	/*Icona immagine*/
+	#toolbarclosedimage{
+		width: 44px;
+		height: 44px;
+	}
+
+	/*NEWNOTE APERTA*/
+	/*Container barra inferiore newnote*/
+	#toolbarcontainer{
+		position: relative;
+		/*margin-top: 32px;*/
+		margin-left: 15px;
+		margin-right: 15px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	/*Container strumenti*/
+	#tools{
+		display: flex;
+		flex-direction: row;
+	}
+
+	/*Icona immagine*/
+	#toolbaropenimage{
+		margin-right: 8px;
+		margin-right: 8px;
+		width: 34px;
+		height: 34px;
+	}
+
+	/*Icona palette*/
+	#toolbaropenpalette{
+		margin-right: 8px;
+		margin-left: 8px;
+		width: 34px;
+		height: 34px;
+	}
+
+	/*TEXTAREA BODY*/
+	/*Container per dare padding alla textarea del body*/
+	#newbodycontainer{
+		display: flex;
+		height: fit-content;
+		padding-top: 12px;
+		padding-bottom: 12px;
+		padding-left: 15px;
+		padding-right: 15px;
+	}
+	/*Proprietà font placeholder body*/
+	#newbody::placeholder{
+		font-size: 14px;
+		color: rgba(0,0,0,0.702);
+		font-weight: 500;
+		letter-spacing: 0.01785714em;
+		line-height: 1.25rem;
+	}
+	/*Proprietà aggiuntive per body nuovo postit*/
+	#newbody{
+		height: var(--height);
+		background-color: var(--background-color);
+		max-height: var(--max-height);
+		overflow: auto;
+		color: #202124;
+		font-size: 14px;
+		letter-spacing: .01428571em;
+		font-weight: 400;
+		line-height: 1.25rem;
+		padding: 0px;
+		border-radius: 0px;
+		margin: 0px;
+       
+        resize: none;
+		width: 100%;
+		border: none;
+		outline: none;
+		white-space: pre-wrap;
+	}
+	
+	/*BOTTONE CHIUDI NOTA*/
+	/*Button chiudi nota*/
+	#closenote{
+		background-color: white;
+		border: transparent;
+		height: 36px;
+		padding: 8px 24px;
+		border-radius: 4px;
+		margin-top: 2px;
+		margin-bottom: 2px;
+		font-size: 14px;
+		color: rgba(0,0,0,.87);
+		font-weight: 500;
+		line-height: 1.25rem;
+		background-color: var(--background-color);
+	}
+	#closenote:hover{
+		background-color: rgba(95,99,104,0.039);
+	}
+	#closenote:active{
+		background-color: rgba(95,99,104,0.161)!important;
+	}
+</style>
